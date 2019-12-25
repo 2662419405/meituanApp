5e311b73eca40d29eac68343344481e36d2741e7
sh <2662419405@qq.com>
2019-12-25 21:07:17 +0800

    :book: 项目初始化

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <Text>
+      这是一段文字定位
+      123
+    </Text>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default App;
