53dd1eb32e824ed7c8d1b08df6c8579a62a736b4
孙航 <2662419405@qq.com>
2020-02-20 12:18:36 +0800

    :tada: 底部导航实现

--- App.js
+++ App.js
@@ -1,113 +1,96 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import TabNavigator from 'react-native-tab-navigator';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
+import {StyleSheet, Text, View, Image} from 'react-native';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'home',
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TabNavigator>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'home'}
+            selectedTitleStyle={{color: 'blue'}}
+            title="首页"
+            renderIcon={() => <Image style={styles.image} />}
+            renderSelectedIcon={() => (
+              <Image style={[styles.image, {tintColor: 'blue'}]} />
+            )}
+            badgeText="1"
+            onPress={() => this.setState({selectedTab: 'home'})}>
+            <View style={styles.page1}>
+              <Text>12332</Text>
             </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'chat'}
+            selectedTitleStyle={{color: 'blue'}}
+            title="聊天"
+            renderIcon={() => <Image style={styles.image} />}
+            renderSelectedIcon={() => (
+              <Image style={[styles.image, {tintColor: 'blue'}]} />
+            )}
+            onPress={() => this.setState({selectedTab: 'chat'})}>
+            <View style={styles.page1}>
+              <Text>12332</Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tong'}
+            selectedTitleStyle={{color: 'blue'}}
+            title="通讯录"
+            renderIcon={() => <Image style={styles.image} />}
+            renderSelectedIcon={() => (
+              <Image style={[styles.image, {tintColor: 'blue'}]} />
+            )}
+            onPress={() => this.setState({selectedTab: 'tong'})}>
+            <View style={styles.page2}></View>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'account'}
+            selectedTitleStyle={{color: 'blue'}}
+            title="设置"
+            renderIcon={() => <Image style={styles.image} />}
+            renderSelectedIcon={() => (
+              <Image style={[styles.image, {tintColor: 'blue'}]} />
+            )}
+            onPress={() => this.setState({selectedTab: 'account'})}>
+            <View style={styles.page3}></View>
+          </TabNavigator.Item>
+        </TabNavigator>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    backgroundColor: '#f5fcff',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  page1: {
+    flex: 1,
+    backgroundColor: 'red',
   },
-  body: {
-    backgroundColor: Colors.white,
+  page2: {
+    flex: 1,
+    backgroundColor: 'yellow',
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  page3: {
+    flex: 1,
+    backgroundColor: 'blue',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  image: {
+    height: 22,
+    width: 22,
   },
 });
 
---
+++ README.md
@@ -0,0 +1,37 @@
+<div align="center">
+
+# React-美团(Dva + umi + koa + Mongoose)
+
+</div>
+
+> **说明** <br/>
+> 自己为何要去尝试写一个这么多页面的网页:100:,本人是纯粹的小白一枚,在各大论坛不停的刷着Vue,React,RN,微信小程序等方面的教程,虽然不停的学习,但是发现自己的技术并没有实质上的提高,就是不停的重复着同样的任务,每个地方都学到了一点,只要混合在一起,马上就全都不会了,:tada:所以第一次尝试使用最新的技术去制作一个大型网站---->至于为何选择了美团,主要是每次看的教程都是做**饿了么**,这次就打算做一个和饿了么类似的美团,数据的话用了美团官网的一部分API,自己写了一部分
+
+[![Build Status](https://www.travis-ci.org/2662419405/meituan.svg?branch=master)](https://www.travis-ci.org/2662419405/meituan)
+
+## 其余端支持
+
+* 美团电脑网页 React + Koa + Mongoose <a href="https://github.com/2662419405/meituan">开发完毕</a> :tada:
+
+* 美团手机网页 Dva + umi + koa + Mongoose <a href="https://github.com/2662419405/meituanAn">开发完毕</a> :100:
+
+* 美团APP ReactNative + koa + Mongoose <a href="https://github.com/2662419405/meituanApp">正在开发</a>
+
+* 微信小程序 Tora <a href="#">还未开发</a>
+
+## 完成
+
+- [x] 项目初始化
+
+## 计划
+
+- [ ] SSR服务端渲染 
+- [ ] 使用TS编写代码 
+- [ ] 底部菜单栏
+- [ ] 登录
+- [ ] 注册
+- [ ] 上传头像
+- [ ] 下单
+- [ ] 添加好友
+- [ ] 进行聊天
+- [ ] jwt
